34f9cc918db96519d305edbb87a427e38230bb8d
chenwoyao <chenwoyao@163.com>
2021-05-25 11:33:28 +0800

    What: 1.'add by woyao'
    Why: 1.
    
    How: 1.

--- fish_web/src/index.js
+++ fish_web/src/index.js
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2020-12-25 10:07:47
- * @LastEditTime: 2020-12-29 23:50:58
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /woyao_cli/src/index.js
- */
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.scss'
